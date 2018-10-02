6f0e686720dcfa8ae50b6cacc9e14117f668fbd2
Daniel Kim <tiger8229@gmail.com>
2018-10-02 18:48:49 +1000

    1021 in progress

--- 4_1021_spin.py
+++ 4_1021_spin.py
@@ -2,7 +2,9 @@
 N, M = map(int, input().split(' '))
 order =[]
 items =[]
+count =0
 orders = input().split(' ')
+pointer =1
 
 for i in range (0,M):
     order.append(int(orders[i]))
@@ -11,8 +13,10 @@ for j in range(0,N):
     items.append(j+1)
 
 for k in range (0,len(order)):
+
+    if k >N
     for l in range(0, len(items)):
-        
-
-
+        if order[k]== items[l]:
+            count += l
+            items.remove(l)
 
