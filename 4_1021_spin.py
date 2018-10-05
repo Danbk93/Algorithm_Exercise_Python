0c9a9a880e77593b684747f7d226db2a5577de63
Daniel Kim <tiger8229@gmail.com>
2018-10-05 16:13:30 +1000

    chaper4 done

--- 4_1021_spin.py
+++ 4_1021_spin.py
@@ -1,22 +1,38 @@
+from collections import deque
+import copy
+
+items = deque()
+order =[]
+count =0
 
 N, M = map(int, input().split(' '))
-order =[]
-items =[]
-count =0
-orders = input().split(' ')
-pointer =1
+for i in range(0,N):
+    items.append(i+1)
 
-for i in range (0,M):
-    order.append(int(orders[i]))
+for j in map(int, input().split(' ')):
+    order.append(j)
 
-for j in range(0,N):
-    items.append(j+1)
+for i in range(len(order)):
 
-for k in range (0,len(order)):
+    left = 0
+    right = 0
 
-    if k >N
-    for l in range(0, len(items)):
-        if order[k]== items[l]:
-            count += l
-            items.remove(l)
+    if items[0] == order[i]:
+        items.popleft()
+    else:
+        items2 = copy.deepcopy(items)
 
+        while items2[0] != order[i]:
+            items2.rotate(1)
+            right += 1
+        while items[0] != order[i]:
+            items.rotate(-1)
+            left += 1
+        if right>=left:
+            count += left
+        else:
+            count += right
+            items=items2
+        items.popleft()
+
+print(count)
